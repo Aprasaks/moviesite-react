38f5c5cabc8040db1c665e6c37ede75fe5fe5d94
Y.KangHyeok <heavenin24@naver.com>
2025-05-08 23:38:33 +0900

    🌱 Vite React Tailwind 초기설정 완료
    - Vite 프로젝트 생성

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Tailwind 설치 성공!</h1>
+    </div>
+  );
+}
+
+export default App;
